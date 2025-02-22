e413dc878826075c7b220befec0597e3d599e7ce
Neha Verma <nehbehl@gmail.com>
2025-02-22 16:04:54 +0530

    Create utils.h

---
+++ header_files/utils.h
@@ -0,0 +1,8 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+// TODO: declare necessary utility/helper functions
+char * create_copy_filename(char *original_filename);
+void display_help();
+void copy_file(char *original_filename, char *new_filename);
+#endif
